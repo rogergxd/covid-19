5afa4199dfe31e4739ba23e66f9e87d9ed21e256
JtorresMorean <jtorres@morean.co>
2020-06-25 17:37:49 -0300

    Updates

---
+++ precache-manifest.dd74a2d503d38fe2dc54ad987747a2be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e52530c76e34e48534a021c178a0127",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "167d6e51c38b87ec85a3",
+    "url": "/covid-19/static/css/main.b9071a9c.chunk.css"
+  },
+  {
+    "revision": "fd8a575060e76e8d3eba",
+    "url": "/covid-19/static/js/2.bba05653.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19/static/js/2.bba05653.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "167d6e51c38b87ec85a3",
+    "url": "/covid-19/static/js/main.afef695a.chunk.js"
+  },
+  {
+    "revision": "1056efa8ca27ab591852",
+    "url": "/covid-19/static/js/runtime-main.50783b27.js"
+  },
+  {
+    "revision": "e96cac3044db7e9b589979b0cdef73cd",
+    "url": "/covid-19/static/media/covid.e96cac30.jpeg"
+  }
+]);
